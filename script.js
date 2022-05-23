e31ecc46c319b69829563e0440d80e4f133cf01d
Иван <v.a.n.y.a.p.e.s.h.k.o.v.1@gmail.com>
2022-05-23 12:46:02 +0700

    генерация меню

--- script.js
+++ script.js
@@ -30,10 +30,39 @@ else {
      }, 2000);
  }
 
+ function generateMenu() {
+    let menu = document.querySelector('nav.main-menu ul');
+    menu.innerHTML = '';
+    let items = [
+       {href: 'index.html', text: 'Товары'},
+       {href: '', text: 'Контакты'},
+       {href: '', text: 'Доставка'},
+       {href: '', text: 'Акции'},
+       {href: '', text: 'О нас'},
+    ];
+ 
+    for(let i = 0; i<items.length; i++){
+       let link = document.createElement('a');
+       link.innerText = items[i].text;
+       link.href = items[i].href;
+       if (items[i].href == '') {
+          link.addEventListener('click', notReadyAlert);
+       }
+ 
+       let menuItem = document.createElement('li');
+       menuItem.appendChild(link);
+ 
+       menu.appendChild(menuItem);
+    }
+ }
+
  function loaded() {
     let searchbox = document.getElementById('search');
     searchbox.addEventListener('keydown', function(key){
        if(key.key == 'Enter')
            search();
     });
+
+    generateMenu();
+    generateCard();
 }
